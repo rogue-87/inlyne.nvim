fcd047d8052e4e1e6041a56c4fc2249ae166e3a5
Rogue <rogue87@tuta.io>
2025-07-24 15:22:33 +0200

    remove status subcommand

--- lua/inlyne/commands/sub_commands/init.lua
+++ lua/inlyne/commands/sub_commands/init.lua
@@ -1,6 +1,5 @@
 local M = {
 	start = require("inlyne.commands.sub_commands.start"),
-	status = require("inlyne.commands.sub_commands.status"),
 	stop = require("inlyne.commands.sub_commands.stop"),
 	toggle = require("inlyne.commands.sub_commands.toggle"),
 }
