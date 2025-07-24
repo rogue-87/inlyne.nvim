6da640b3edb12b9f03655ddb71e6cf1c12894d8f
Rogue <rogue87@tuta.io>
2025-07-24 12:40:42 +0200

    implement toggle subcommand

--- lua/inlyne/commands/sub_commands/toggle.lua
+++ lua/inlyne/commands/sub_commands/toggle.lua
@@ -1,5 +1,18 @@
+local runner = require("inlyne.lib.runner")
+local temp = require("inlyne.lib.temp")
+
 local M = {}
 
-function M.impl(args, opts) end
+function M.impl(args, opts)
+	if runner.is_running then
+		runner:stop()
+		temp:close()
+		vim.notify("Inlyne stopped", vim.log.levels.INFO)
+	else
+		temp:create()
+		runner:execute()
+		vim.notify("Inlyne started", vim.log.levels.INFO)
+	end
+end
 
 return M
