db492923f5af673bcd149372da4e46f57a4360cd
Rogue <rogue87@tuta.io>
2025-07-25 19:54:37 +0200

    update repro.lua file

--- repro.lua
+++ repro.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 -- Run with `nvim -u repro.lua`
 vim.env.LAZY_STDPATH = ".repro"
 load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
@@ -10,6 +13,11 @@ require("lazy.minit").repro({
 			dir = "~/Code/personal/inlyne.nvim/",
 			dev = true,
 			opts = {},
+			keys = {
+				{ "<leader>ie", "<cmd>Inlyne enable<cr>", desc = "Enable Inlyne" },
+				{ "<leader>id", "<cmd>Inlyne disable<cr>", desc = "Disable Inlyne" },
+				{ "<leader>it", "<cmd>Inlyne toggle<cr>", desc = "Toggle Inlyne" },
+			},
 		},
 	},
 })
