a1bed9d1f248d570c50b3a38d28278793e94218f
Rogue <rogue87@tuta.io>
2025-07-25 18:39:47 +0200

    fix: check if current buffer is a markdown file

--- lua/inlyne/commands/sub_commands/enable.lua
+++ lua/inlyne/commands/sub_commands/enable.lua
@@ -1,11 +1,21 @@
 local temp = require("inlyne.lib.temp")
 local runner = require("inlyne.lib.runner")
 
+local function is_markdown_buffer()
+	local bufnr = vim.api.nvim_get_current_buf()
+	return vim.bo[bufnr].filetype == "markdown"
+end
+
 local M = {}
 
 function M.impl()
 	if runner.is_running then
 		vim.notify("Inlyne is already running", vim.log.levels.INFO)
+		return
+	end
+
+	if not is_markdown_buffer() then
+		vim.notify("Currently opened file is not a markdown file", vim.log.levels.INFO)
 		return
 	end
 
